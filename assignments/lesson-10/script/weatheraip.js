8ccd7b842a95d5206ef48f7239c92d59d847713d
kalbert83 <mat17031@byui.edu>
2019-03-13 18:51:24 -0600

    update api activity

--- assignments/lesson-10/script/weatheraip.js
+++ assignments/lesson-10/script/weatheraip.js
@@ -1,6 +1,6 @@
 var span = document.getElementById("current-temp");
 let weatherRequest = new XMLHttpRequest();
-var apiURLstring = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=2193c20ec75fcfb06c28f61f81d5bfc1';
+var apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=2193c20ec75fcfb06c28f61f81d5bfc1';
 weatherRequest.open('GET', apiURLstring);
 weatherRequest.onload =  function () {
     let weatherData = JSON.parse(weatherRequest.responseText);
