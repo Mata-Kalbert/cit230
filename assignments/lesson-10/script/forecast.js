5a368099c6f179bcf32890f245b9b8d6a10ff4bb
kalbert83 <mat17031@byui.edu>
2019-03-18 17:07:07 -0600

    weather update

---
+++ assignments/lesson-10/script/forecast.js
@@ -0,0 +1,34 @@
+let fRequest = new XMLHttpRequest();
+let apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=2193c20ec75fcfb06c28f61f81d5bfc1';
+fRequest.open('GET', apiURL);
+fRequest.send();
+fRequest.onload =  function () {
+    let weatherData = JSON.parse(fRequest.responseText);
+    console.log(weatherData);
+
+    let tempForecast = [];
+    let dayWeek = [];
+    let icon = [];
+    let day = 1;
+    
+    for (let i = 0; i < weatherData.list.length;  i++) {
+        if (weatherData.list[i].dt_txt.includes('18:00:00')) {
+            let date = new Date(weatherData.list[i].dt * 1000);
+            let dayOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+            let findDate = dayOfWeek[date.getDay()];
+            dayWeek.push(findDate);
+
+            tempForecast[day] = weatherData.list[i].main.temp;
+            day++;
+
+        }
+
+    }
+    for ( let i = 1; i <=5; i++) {
+        document.getElementById('temp'+ i).innerHTML = tempForecast[i];
+        document.getElementById('day' + i).innerHTML = dayWeek[i - 1];
+
+
+
+    }
+}
