7ce096c5e0c9d85191c139b20da9fee62f0fdadb
kalbert83 <mat17031@byui.edu>
2019-03-21 23:31:34 -0600

    weather update

---
+++ assignments/weathersite/script/fishhavenweather.js
@@ -0,0 +1,29 @@
+let weatherRequest = new XMLHttpRequest();
+var apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5585010&units=imperial&APPID=2193c20ec75fcfb06c28f61f81d5bfc1';
+weatherRequest.open('GET', apiURLstring);
+weatherRequest.onload =  function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+
+    var span = document.getElementById("current");
+    span.textContent = weatherData.weather[0].description;
+    var spanTwo = document.getElementById("high");
+    spanTwo.textContent = weatherData.main.temp_max;
+    var spanThree = document.getElementById("humidity");
+    spanThree.textContent = weatherData.main.humidity;
+    var spanfour = document.getElementById("speed");
+    spanfour.textContent = weatherData.wind.speed;
+
+    var temp = weatherData.main.temp;
+    var speed = parseFloat(document.getElementById("speed").innerHTML);
+    var compute = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16)
+        + 0.4275 * temp * Math.pow(speed, 0.16);
+
+    document.getElementById("output").innerHTML = compute.toFixed(1);
+    
+
+
+}
+
+weatherRequest.send();
---
+++ assignments/weathersite/script/sodaspringsevents.js
@@ -0,0 +1,34 @@
+let events = document.getElementById("events");
+    let requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() {
+    let event = request.response;
+    populate(event);
+    
+
+    function populate(event) {
+        let townEvents = event['towns'];
+        for (let i = 0; i < townEvents.length; i++) {
+            if (townEvents[i].name === 'Soda Springs') {
+                let article = document.createElement("article");
+                let ul = document.createElement("ul");
+                let eventsUl = townEvents[i].events;
+
+                for (let c = 0; c < eventsUl.length; c++) {
+                    let ulItems = document.createElement("li");
+                    ulItems.textContent = eventsUl[c];
+                    ul.appendChild(ulItems);
+
+                }
+                article.appendChild(ul);
+                events.appendChild(article);
+            }
+
+                
+        }
+        }
+    }
